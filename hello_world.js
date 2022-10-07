6bafe67987267dc76351ccaa7f2df7e899410704
cabralcaio <caio_cabral1@hotmail.com>
2022-10-07 16:40:14 -0300

    Aula 07/10 completa

--- hello_world.js
+++ hello_world.js
@@ -1,22 +1,18 @@
+"use strict";
 // tipagem no TypeScript
-
 // tipagem implícita
-
 let username = 'shaolin_matador_de_porco';
 // username = 6;   acusa erro no typescript porque o tipo implicito da variavel é string
 username = 'flavioDoPneu';
-
 // tipagem explícita
-let pi: number = Math.PI;
+let pi = Math.PI;
 // pi = 'suco Tang'; acusa erro no typescript porque o tipo da variável é number e atribui uma string
-let logado: boolean = true;
-
+let logado = true;
 // union types
-let rg: number | string= 12345678900;
+let rg = 12345678900;
 rg = '3983847638';
-
 // qualquer tipo: any
-let generico: any = [];
+let generico = [];
 generico = {
     a: '',
     b: 8,
@@ -24,16 +20,14 @@ generico = {
 };
 generico = 'eu sou genérico';
 generico = 6.89;
-
 // maneira 1 de se declarar
-const listaDeNomes: string[] = ['Jose', 'Carlos', 'Eduardo', 'Maria'];
+const listaDeNomes = ['Jose', 'Carlos', 'Eduardo', 'Maria'];
 listaDeNomes.push('Camila');
-const notas: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const notas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // notas.push('e ai cara'); acusa ERRO
-
 // maneira 2 de se declarar
-const valores: Array<string | number> = ['pudim', 96, 'batata', 999];
-const array: Array<boolean> = [true, false, false, false, true, true, false];
-const lista: Array<any> = [1, true, {}, [], 'sadasda'];
-let listaPodeTudo: Array<any> = [true,'alo',2.3,8,"b"];
+const valores = ['pudim', 96, 'batata', 999];
+const array = [true, false, false, false, true, true, false];
+const lista = [1, true, {}, [], 'sadasda'];
+let listaPodeTudo = [true, 'alo', 2.3, 8, "b"];
 console.log(listaPodeTudo);
